3c54ece9b8c382759977bb6dea42e1f9378fb499
Kyler Scheer <kyler-23@hotmail.com>
2024-10-22 18:32:31 -0500

    initial commit

---
+++ LetsLearnSwift/LetsLearnSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  LetsLearnSwiftApp.swift
+//  LetsLearnSwift
+//
+//  Created by Kyler Scheer on 10/22/24.
+//
+
+import SwiftUI
+
+@main
+struct LetsLearnSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
